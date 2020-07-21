6b5f6a2d6f1a8613f94e38603796735010767e7c
Nikita Shidlovsky <nik-801@yandex.ru>
2020-07-21 14:52:42 +0300

    Updates

---
+++ precache-manifest.60d57e490319ae7d141838657528b7e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "634788c16f65a04efed09877bbc3aee9",
+    "url": "/vk-apps-helloworld/index.html"
+  },
+  {
+    "revision": "a65d5c5de61c404cd1c9",
+    "url": "/vk-apps-helloworld/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "1a8d175371ed59abae71",
+    "url": "/vk-apps-helloworld/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "a65d5c5de61c404cd1c9",
+    "url": "/vk-apps-helloworld/static/js/2.a292c3fa.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/vk-apps-helloworld/static/js/2.a292c3fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a8d175371ed59abae71",
+    "url": "/vk-apps-helloworld/static/js/main.fdc03732.chunk.js"
+  },
+  {
+    "revision": "58de19f4cfd7ab7cd646",
+    "url": "/vk-apps-helloworld/static/js/runtime-main.5b947f57.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/vk-apps-helloworld/static/media/persik.4e1ec840.png"
+  }
+]);
